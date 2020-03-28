42f151dc68f1265336f2be6ec65c98cd06d88039
Urszula Ryszawa <s17452@pjwstk.edu.pl>
2020-03-28 19:53:01 +0100

    crawling emails

--- Zad1/ConsoleApp1/Program.cs
+++ Zad1/ConsoleApp1/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(args[0]);
+
+            if (response.IsSuccessStatusCode) //2xx
+            {
+                var html = await response.Content.ReadAsStringAsync();
+                var regex = new Regex("[a-zA-Z0-9]+@[a-z.]+");
+                var matches = regex.Matches(html);
+                foreach(var i in matches)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.WriteLine("Koniec!");
         }
     }
 }
